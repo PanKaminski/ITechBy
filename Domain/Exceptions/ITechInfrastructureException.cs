0cc823531f2e55af0f3284493349460ae3841ecd
Fiodar Kaminski <ksendzfs@gmail.com>
2023-07-25 23:06:12 +0300

    :sparkles: implement baseic repository

---
+++ Domain/Exceptions/ITechInfrastructureException.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.Exceptions
+{
+    public class ITechInfrastructureException : Exception
+    {
+        public string[] Parameters { get; set; } = new string[0];
+        public ExceptionCode ExceptionCode { get; set; }
+
+        public ITechInfrastructureException(string message) : base(message) { }
+
+        public ITechInfrastructureException(ExceptionCode code, string message) : base(message) 
+        {
+            ExceptionCode = code;
+        }
+
+        public ITechInfrastructureException(ExceptionCode code) : this(code, string.Empty)
+        {
+        }
+
+        public ITechInfrastructureException(ExceptionCode code, string message, params string[] parameters) : this(code, message)
+        {
+            Parameters = parameters;
+        }
+
+        public ITechInfrastructureException(string message, params string[] parameters) : base(message) 
+        { 
+            Parameters =  parameters;
+        }
+
+        public ITechInfrastructureException(string message, Exception inner) : base(message, inner) { }
+
+    }
+}
---
+++ Infrastructure.Implementations/Repositories/RepositoryBase.cs
@@ -0,0 +1,85 @@
+﻿using Domain.Exceptions;
+using Infrastructure.Implementations.DataMaster;
+using Infrastructure.Implementations.Extensions;
+using ITechBy.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Services.Abstractions.Data.Repositories;
+
+namespace Infrastructure.Implementations.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepository<T> where T : AuditableEntity
+    {
+        protected ITechDbContext DbContext { get; }
+
+        public RepositoryBase(ITechDbContext context) => DbContext = context ?? throw new ArgumentNullException(nameof(context));
+
+        public void Create(T entity)
+        {
+            ArgumentNullException.ThrowIfNull(entity, nameof(entity));
+
+            entity.CreatedTime = DateTime.UtcNow;
+
+            DbContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var entity = DbContext.Set<T>().Find(id);
+
+            if (entity is null)
+            {
+                throw new ITechInfrastructureException(ExceptionCode.ItemDoesntExist);
+            }
+
+            DbContext.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> Get(Predicate<T> condition, bool trackEntities)
+        {
+            IQueryable<T> model = DbContext.Set<T>().Where(item => condition(item));
+
+            if (!trackEntities)
+            {
+                model = model.AsNoTracking();
+            }
+
+            return model;
+        }
+
+        public IQueryable<T> Get(Predicate<T> condition, int skipCount, int count, bool trackEntities)
+        {
+            if (skipCount < 0)
+            {
+                throw new ArgumentException("Count of items to skip is less than zero.");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentException("Count of items to take is less than zero.");
+            }
+
+            return Get(condition, trackEntities).Skip(skipCount).Take(count);
+        }
+
+        public IAsyncEnumerable<T> GetAllAsync() => DbContext.Set<T>().AsNoTracking().AsAsyncEnumerable();
+
+        public IAsyncEnumerable<T> GetAsync(Predicate<T> condition, int skipCount, int count, bool trackEntities) =>
+            Get(condition, skipCount, count, trackEntities).AsAsyncEnumerable();
+
+        public IAsyncEnumerable<T> GetAsync(Predicate<T> condition, bool trackEntities) =>
+            Get(condition, trackEntities).AsAsyncEnumerable();
+
+
+        public async Task<PagedModel<T>> GetPaginatedAsync(Predicate<T> condition, int page, int pageSize, bool trackEntities)
+        {
+            return await (Get(condition, trackEntities)).PaginateAsync(page, pageSize);
+        }
+
+        public void Update(T entity)
+        {
+            entity.UpdatedTime = DateTime.UtcNow;
+
+            DbContext.Set<T>().Update(entity);
+        }
+    }
+}
